099c9778980cecaa248136b62cf2148718e8c2a7
jeusunei <marco.anello@gmail.com>
2017-01-08 18:33:11 +0100

    adding DevTMUX

---
+++ DevTMUX.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+tmux new-session -d -c ~/Documents/Projects/Programming -n programming -s deve 
+tmux selectp -t deve
+tmux split-window -h -c ~/Documents/Projects/Programming -p 40 
+tmux split-window -v -c ~/Documents/Projects/Programming -p 60
+tmux new-window -c ~/Documents/Projects/Programming -n reference
+tmux new-window -c ~/WorkSpaces/CheatSheet -n CheatSheet 
+
+tmux send-keys -t deve:0.1 'g++ *.cpp -D DEBUG -o test' C-m
+
+tmux send-keys -t deve:0.0 'clear' C-m
+tmux send-keys -t deve:0.1 'clear' C-m
+tmux send-keys -t deve:0.2 'clear' C-m
+
+tmux send-keys -t deve:1.0 'clear' C-m
+tmux send-keys -t deve:2.0 'clear' C-m
+
+tmux send-keys -t deve:2.0 'ls -l' C-m
+
+tmux choose-window -t programming
+tmux attach -t deve
+
