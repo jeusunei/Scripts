442d540980f2655167cdfae1f904f4119b70fd4d
jeusunei <marco.anello@gmail.com>
2022-06-19 23:38:10 +0200

    Update DevTMUX.sh

--- DevTMUX.sh
+++ DevTMUX.sh
@@ -6,7 +6,7 @@ tmux split-window -v -c ~/Documents/Projects/Programming -p 60
 tmux new-window -c ~/Documents/Projects/Programming -n reference
 tmux new-window -c ~/WorkSpaces/CheatSheet -n CheatSheet 
 
-tmux send-keys -t deve:0.1 'g++ *.cpp -D DEBUG -o test' C-m
+ttmux send-keys -t deve:0.1 'g++ *.cpp -D DEBUG -o a.out 2> errorListing.dat' C-m
 
 tmux send-keys -t deve:0.0 'clear' C-m
 tmux send-keys -t deve:0.1 'clear' C-m
@@ -19,4 +19,3 @@ tmux send-keys -t deve:2.0 'ls -l' C-m
 
 tmux choose-window -t programming
 tmux attach -t deve
-
